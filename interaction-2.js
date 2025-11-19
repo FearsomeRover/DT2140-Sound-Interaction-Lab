1726b4415195b33a6b7b8af797a0ce9e7ce602ce
FearsomeRover <bujgergo@gmail.com>
2025-11-19 13:32:34 +0100

    bigger range

--- interaction-2.js
+++ interaction-2.js
@@ -56,7 +56,7 @@ function accelerationChange(accx, accy, accz) {
 
 function rotationChange(rotx, roty, rotz) {
   console.log(rotx, roty, rotz);
-  if (rotx > 130 && rotx < 140 && roty > 80 && roty < 90 && !soundPlaying) {
+  if (rotx > 120 && rotx < 150 && roty > 70 && roty < 100 && !soundPlaying) {
     playAudio();
     soundPlaying = true;
   } else {
