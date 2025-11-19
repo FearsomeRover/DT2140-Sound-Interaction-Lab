f10119fc46686150f73f26851d180ef004bdad1d
FearsomeRover <bujgergo@gmail.com>
2025-11-19 13:58:14 +0100

    door

--- interaction-3.js
+++ interaction-3.js
@@ -12,33 +12,31 @@ let dspNodeParams = null;
 let jsonParams = null;
 
 // Change here to ("tuono") depending on your wasm file name
-const dspName = "brass";
+const dspName = "door";
 const instance = new FaustWasm2ScriptProcessor(dspName);
 
 // output to window or npm package module
 if (typeof module === "undefined") {
-    window[dspName] = instance;
+  window[dspName] = instance;
 } else {
-    const exp = {};
-    exp[dspName] = instance;
-    module.exports = exp;
+  const exp = {};
+  exp[dspName] = instance;
+  module.exports = exp;
 }
 
 // The name should be the same as the WASM file, so change tuono with brass if you use brass.wasm
-brass.createDSP(audioContext, 1024)
-    .then(node => {
-        dspNode = node;
-        dspNode.connect(audioContext.destination);
-        console.log('params: ', dspNode.getParams());
-        const jsonString = dspNode.getJSON();
-        jsonParams = JSON.parse(jsonString)["ui"][0]["items"];
-        dspNodeParams = jsonParams
-        // const exampleMinMaxParam = findByAddress(dspNodeParams, "/thunder/rumble");
-        // // ALWAYS PAY ATTENTION TO MIN AND MAX, ELSE YOU MAY GET REALLY HIGH VOLUMES FROM YOUR SPEAKERS
-        // const [exampleMinValue, exampleMaxValue] = getParamMinMax(exampleMinMaxParam);
-        // console.log('Min value:', exampleMinValue, 'Max value:', exampleMaxValue);
-    });
-
+door.createDSP(audioContext, 1024).then((node) => {
+  dspNode = node;
+  dspNode.connect(audioContext.destination);
+  console.log("params: ", dspNode.getParams());
+  const jsonString = dspNode.getJSON();
+  jsonParams = JSON.parse(jsonString)["ui"][0]["items"];
+  dspNodeParams = jsonParams;
+  // const exampleMinMaxParam = findByAddress(dspNodeParams, "/thunder/rumble");
+  // // ALWAYS PAY ATTENTION TO MIN AND MAX, ELSE YOU MAY GET REALLY HIGH VOLUMES FROM YOUR SPEAKERS
+  // const [exampleMinValue, exampleMaxValue] = getParamMinMax(exampleMinMaxParam);
+  // console.log('Min value:', exampleMinValue, 'Max value:', exampleMaxValue);
+});
 
 //==========================================================================================
 // INTERACTIONS
@@ -52,37 +50,45 @@ brass.createDSP(audioContext, 1024)
 //==========================================================================================
 
 function accelerationChange(accx, accy, accz) {
-    // playAudio()
+  // playAudio()
 }
 
+let doorOpened = false;
 function rotationChange(rotx, roty, rotz) {
+  if (rotz > 178 && rotz < 182 && !doorOpened) {
+    playAudio();
+    doorOpened = true;
+    setTimeout(() => {
+      doorOpened = false;
+    }, 2000);
+  }
 }
 
 function mousePressed() {
-    playAudio(mouseX/windowWidth)
-    // Use this for debugging from the desktop!
+  playAudio(mouseX / windowWidth);
+  // Use this for debugging from the desktop!
 }
 
 function deviceMoved() {
-    movetimer = millis();
-    statusLabels[2].style("color", "pink");
+  movetimer = millis();
+  statusLabels[2].style("color", "pink");
 }
 
 function deviceTurned() {
-    threshVals[1] = turnAxis;
+  threshVals[1] = turnAxis;
 }
 function deviceShaken() {
-    shaketimer = millis();
-    statusLabels[0].style("color", "pink");
-    playAudio();
+  shaketimer = millis();
+  statusLabels[0].style("color", "pink");
+  playAudio();
 }
 
 function getMinMaxParam(address) {
-    const exampleMinMaxParam = findByAddress(dspNodeParams, address);
-    // ALWAYS PAY ATTENTION TO MIN AND MAX, ELSE YOU MAY GET REALLY HIGH VOLUMES FROM YOUR SPEAKERS
-    const [exampleMinValue, exampleMaxValue] = getParamMinMax(exampleMinMaxParam);
-    console.log('Min value:', exampleMinValue, 'Max value:', exampleMaxValue);
-    return [exampleMinValue, exampleMaxValue]
+  const exampleMinMaxParam = findByAddress(dspNodeParams, address);
+  // ALWAYS PAY ATTENTION TO MIN AND MAX, ELSE YOU MAY GET REALLY HIGH VOLUMES FROM YOUR SPEAKERS
+  const [exampleMinValue, exampleMaxValue] = getParamMinMax(exampleMinMaxParam);
+  console.log("Min value:", exampleMinValue, "Max value:", exampleMaxValue);
+  return [exampleMinValue, exampleMaxValue];
 }
 
 //==========================================================================================
@@ -90,20 +96,20 @@ function getMinMaxParam(address) {
 //------------------------------------------------------------------------------------------
 //
 //------------------------------------------------------------------------------------------
-// Edit here to define your audio controls 
+// Edit here to define your audio controls
 //------------------------------------------------------------------------------------------
 //
 //==========================================================================================
 
 function playAudio(pressure) {
-    if (!dspNode) {
-        return;
-    }
-    if (audioContext.state === 'suspended') {
-        return;
-    }
-    console.log(pressure)
-    dspNode.setParamValue("/brass/blower/pressure", pressure)
+  if (!dspNode) {
+    return;
+  }
+  if (audioContext.state === "suspended") {
+    return;
+  }
+  console.log(pressure);
+  dspNode.setParamValue("/brass/blower/pressure", pressure);
 }
 
 //==========================================================================================
